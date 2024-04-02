1f4059cfab1a789280b767eaf9c0c3fd1604afde
VictorGuedesJS <victorguedes.js@gmail.com>
2024-04-02 19:22:19 -0400

    To do list html e css

---
+++ TODOLIST/main.js
@@ -0,0 +1,16 @@
+
+
+function createrow(nomeTarefa){
+
+    const tr = createElement("tr")
+    cons
+
+}
+
+document.getElementById("adicionar").addEventListener("click",function(){
+
+    var nomeTarefa = document.getElementById("nomeTarefa").value;
+    createrow(nomeTarefa);
+
+})
+
