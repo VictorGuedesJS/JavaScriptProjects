776d2f2b6c61cf98b626136c2b45b126127256ca
VictorGuedesJS <victorguedes.js@gmail.com>
2024-04-03 18:57:45 -0400

    try do create row

--- TODOLIST/main.js
+++ TODOLIST/main.js
@@ -2,14 +2,36 @@
 
 function createrow(nomeTarefa){
 
-    const tr = createElement("tr")
-    const td = createElement("td", nomeTarefa)
+    const tbody = document.getElementById("tabela");
+    const tr = document.createElement("tr");
+    const td = document.createElement("td", nomeTarefa);
+    var dataAtual = new Date();
+    const data = document.createElement("td", dataAtual.getDate())
+    const  addStatus = document.createElement("td")
+    const  addAction = document.createElement("td")
+
+
+    const editButton = document.createElement("button")
+    editButton.innerHTML = '<span class="material-symbols-outlined">edit</span>'
+
+    addAction.appendChild(editButton)
+    tr.appendChild(td)
+    tbody.appendChild(tr)
+
+    const editSpan =  document.createElement("span")
+
 }
 
 document.getElementById("adicionar").addEventListener("click",function(){
 
-    var nomeTarefa = document.getElementById("nomeTarefa").value;
-    createrow(nomeTarefa);
+   var nomeTarefa = document.getElementById("nomeTarefa").value;
+
 
 })
+
+if (nomeTarefa != null) {
+    
+    createrow(nomeTarefa)
+    
+}
 
